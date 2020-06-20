e6a65ba4cb536edc9a128a56de08ff4d6003f903
Ngoc Duy Bui <13392511@student.uts.edu.au>
2020-06-20 08:18:39 +0700

    5th commit

--- day2_14062020_JSON/demo_json_2.py
+++ day2_14062020_JSON/demo_json_2.py
@@ -2,6 +2,7 @@ import urllib.request,json
 url =urllib.request.urlopen("http://lntpython.laptrinhpython.net:8181/dich-vu-san-pham/chi-tiet")
 noidung = json.loads(url.read().decode())
 stt =0
+print (("DANH SÁCH SẢN PHẨM ("+str(len(noidung))+ ")").center(5+30+50+30))
 print ("STT".ljust(5),"TÊN SẢN PHẨM".ljust(30),"HÌNH SẢN PHẨM".ljust(50),"GIÁ SIZE S".ljust(15),"GIÁ SIZE M".ljust(15))
 print ("-"*(5+30+50+30))
 for i in noidung:
